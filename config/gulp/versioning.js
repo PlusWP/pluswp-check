270e587336ddca94c2ef754452910ac44fcf047d
kuus <kunderikuus@gmail.com>
2015-08-31 16:37:00 +0200

    workflow

--- config/gulp/versioning.js
+++ config/gulp/versioning.js
@@ -8,7 +8,7 @@
 // var exec = require('child_process').exec;
 // var changelog = require('conventional-changelog');
 
-VAR CONFIG = require('./common-config');
+var CONFIG = require('./common-config');
 
 /**
  * Versioning tasks
@@ -19,22 +19,18 @@ VAR CONFIG = require('./common-config');
  * gulp feature   # makes v0.1.1 → v0.2.0
  * gulp release   # makes v0.2.1 → v1.0.0
  *
+ * @access public
  */
-function versioning() {
+gulp.task('versioning', function () {
   var importance;
-  if (CONFIG.argv.patch) importance = 'patch';
-  if (CONFIG.argv.minor) importance = 'minor';
-  if (CONFIG.argv.major) importance = 'major';
+  if (CONFIG.argv.patch) { importance = 'patch'; }
+  if (CONFIG.argv.minor) { importance = 'minor'; }
+  if (CONFIG.argv.major) { importance = 'major'; }
   return gulp.src('./package.json')
     .pipe($.bump({ type: importance }))
-    .pipe(gulp.dest('./'))
+    .pipe(gulp.dest('./'));
     // commit the changed version number
     // .pipe($.git.commit('bumps package version'))
     // tag it in the repository
     // .pipe($.tagVersion());
-}
-
-// @access public
-gulp.task('versioning', function () {
-  return versioning();
 });
